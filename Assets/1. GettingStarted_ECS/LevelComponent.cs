b6e3c4588e12f3643db3fa0e805018cd15d13ff0
A.Madalin <antonoiumadalin@gmail.com>
2020-07-06 15:26:59 +0300

    C.2 Ready - Next is C.3 Systems

---
+++ Assets/1. GettingStarted_ECS/LevelComponent.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities; // import Entities
+
+public struct LevelComponent : IComponentData { // We extend IComponentData whenever we want to use this as a Component. Always struct, not class.
+    
+    public float level;
+    // That's it. Components hold only data, no behavior. We define its behavior in Testing.cs
+
+}
--- Assets/1. GettingStarted_ECS/Testing.cs
+++ Assets/1. GettingStarted_ECS/Testing.cs
@@ -5,11 +5,18 @@ using Unity.Entities; // Use Entities package
 
 public class Testing : MonoBehaviour {
 
-    
     private void Start() {
 
-        EntityManager manager = new EntityManager(); // 1. Define "manager" variable of type Entity Manager to be a new Entity Manager for this script.
-        manager.CreateEntity();                      // 2. Create a new Entity.
+        EntityManager manager = World.DefaultGameObjectInjectionWorld.EntityManager; // 1. Define "manager" variable of type Entity Manager to be a new Entity Manager for this script.
+
+        /* 2. a. Create a new Entity. (manager.CreateEntity();)
+         * +  b. Create the Entity with a component of type LevelComponent (.CreateEntity(typeof(LevelComponent))
+         * +  c. Create a reference to the entity ( Entity entity = ... )
+        */
+        Entity entity = manager.CreateEntity(typeof(LevelComponent)); 
+
+        /*Use the manager to set the component data of LevelComponent to be level 10 for the entity declared above*/
+        manager.SetComponentData(entity, new LevelComponent { level = 10 });
 
     }
 
