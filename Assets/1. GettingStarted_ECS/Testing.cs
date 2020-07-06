23bb0330138b1cc0768aa99ba003cb9fdcf55c1e
A.Madalin <antonoiumadalin@gmail.com>
2020-07-06 13:18:25 +0300

    3. Created our first Entity. Default bare bones

--- Assets/1. GettingStarted_ECS/Testing.cs
+++ Assets/1. GettingStarted_ECS/Testing.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Entities; // Use Entities package
 
 public class Testing : MonoBehaviour {
 
-	void Start(){
-	    
-	}
-	
-	void Update(){
-	    
-	}
+    
+    private void Start() {
+
+        EntityManager manager = new EntityManager(); // 1. Define "manager" variable of type Entity Manager to be a new Entity Manager for this script.
+        manager.CreateEntity();                      // 2. Create a new Entity.
+
+    }
 
 }
