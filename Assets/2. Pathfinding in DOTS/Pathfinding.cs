0a407c91f18238c4920d56de9dcafc546aef2cf4
A.Madalin <antonoiumadalin@gmail.com>
2020-07-07 08:32:23 +0300

    Base variables for Pathfinding.

--- Assets/2. Pathfinding in DOTS/Pathfinding.cs
+++ Assets/2. Pathfinding in DOTS/Pathfinding.cs
@@ -1,19 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Pathfinding : MonoBehaviour {
 
-	void Start(){
-	    
-	}
-	
-	void Update(){
-	    
-	}
 
+	// So, the first thing we need for Pathfinding is to store data on a grid
 	private struct PathNode {
 
+		// X axis, y Axis and each node index
+		public int x, y, index; 
+
+		/* A* Pathfinding Costs 
+			gCost - Move cost from start node onto this node.
+			hCost - Estimated cost from this node onto end node.
+			fCost - Simply G + H.
+		 */
+		public int gCost, hCost, fCost;
+
+		public bool isWalkable; // To determine can walk or not on a particular node
+
+		public int cameFromIndex; // For reverse pathfinding, previous index to hCost
     }
 
 }
