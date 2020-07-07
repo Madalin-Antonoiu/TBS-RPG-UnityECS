f3d97cae4f2f43fac8a6efee36b8c453d9ce4b86
A.Madalin <antonoiumadalin@gmail.com>
2020-07-07 08:24:31 +0300

    2. Pathfinding in DOTS a. Getting Started

---
+++ Assets/2. Pathfinding in DOTS/Pathfinding.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour {
+
+	void Start(){
+	    
+	}
+	
+	void Update(){
+	    
+	}
+
+	private struct PathNode {
+
+    }
+
+}
