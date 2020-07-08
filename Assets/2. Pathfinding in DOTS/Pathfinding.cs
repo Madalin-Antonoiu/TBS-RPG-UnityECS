52946ac6c27b1e4c41062a6f57722958222320ba
A.Madalin <antonoiumadalin@gmail.com>
2020-07-08 12:05:27 +0300

    Pathfinding COMPLETE, with Obstacles

--- Assets/2. Pathfinding in DOTS/Pathfinding.cs
+++ Assets/2. Pathfinding in DOTS/Pathfinding.cs
@@ -2,13 +2,19 @@
 using Unity.Mathematics; // for int2
 using Unity.Collections; // to create our grid into NativeArray
 using System.Security.Cryptography;
+using System.Collections.Generic;
+using Unity.Entities.UniversalDelegates;
 
 public class Pathfinding : MonoBehaviour {
 
 	private const int MOVE_STRAIGHT_COST = 10;
 	private const int MOVE_DIAGONAL_COST = 14;
 
-	/* ------------------2. Create a testing function -----------------------------
+    private void Start() {
+		FindPath(new int2(0, 0), new int2(3, 1));
+    }
+
+    /* ------------------2. Create a testing function -----------------------------
 		THE GOAL: Find the most cost-effective path from A to B.
 		FindPath(a, b) { 
 			startPos - The starting position of our subject (A).
@@ -24,7 +30,7 @@ public class Pathfinding : MonoBehaviour {
 				...
 		}
 		*/
-	private void FindPath(int2 startPos, int2 endPos) {
+    private void FindPath(int2 startPos, int2 endPos) {
 
         // a. ----- Setting up the Grid ----
 		
@@ -38,22 +44,148 @@ public class Pathfinding : MonoBehaviour {
 				node.y = n;
 				node.index = CalculateIndex(m, n, gridSize.x);
 				node.gCost = int.MaxValue;
-				node.hCost = CalculateHCost(new int2(m, n), endPos);
+				node.hCost = CalculateDistanceCost(new int2(m, n), endPos);
 				node.CalculateFCost();
-				node.bIsWalkable = true;
+				node.isWalkable = true;
 				node.cameFromIndex = -1;
 
 				grid[node.index] = node;
 			}
 		}
 
-		// b. Grab the start node
+		// Testing time: Add some walls (obstacles) at (1,0), (1,1) and (1,2)!
+        {
+			PathNode walkablePathNode = grid[CalculateIndex(1, 0, gridSize.x)];
+			walkablePathNode.SetIsWalkable(false);
+			grid[CalculateIndex(1, 0, gridSize.x)] = walkablePathNode;
+
+			walkablePathNode = grid[CalculateIndex(1, 1, gridSize.x)];
+			walkablePathNode.SetIsWalkable(false);
+			grid[CalculateIndex(1, 1, gridSize.x)] = walkablePathNode;
+
+			walkablePathNode = grid[CalculateIndex(1, 2, gridSize.x)];
+			walkablePathNode.SetIsWalkable(false);
+			grid[CalculateIndex(1, 2, gridSize.x)] = walkablePathNode;
 
 
+		}
 
+		// a. Neighbours and the end node
+		NativeArray<int2> neighbours = new NativeArray<int2>(new int2[] {
+			new int2( -1, 0), // Left			⬅️
+			new int2( +1, 0), // Right			➡️
+			new int2( 0, -1), // Down			⬇️
+			new int2( 0, +1), // Up				⬆️
+			new int2( -1, -1), // Left Down		↙️
+			new int2( -1, +1), // Left Up		↖️
+			new int2( +1, -1), // Right Down	↘️
+			new int2( +1, +1), // Right Up		↗️
+		}, Allocator.Temp);
+
+		int endNodeIndex = CalculateIndex(endPos.x, endPos.y, gridSize.x);
+
+		// b. Define the startNode
+		PathNode startNode = grid[CalculateIndex(startPos.x, startPos.y, gridSize.x)];
+		startNode.gCost = 0;
+		startNode.CalculateFCost();
+		grid[startNode.index] = startNode;
+
+		// c. openList and closedList <--- next
+		NativeList<int> openList = new NativeList<int>(Allocator.Temp); // List of indexes
+		NativeList<int> closedList = new NativeList<int>(Allocator.Temp); // List of indexes
+		openList.Add(startNode.index); // Open list to include the startNode
+
+
+		/* This is the motor that keeps doing the work 
+		 * While openList still have cells to search
+		 */
+		while(openList.Length > 0) {
+
+		// Current Node definition
+            int currentNodeIndex = GetLowestCostFNodeIndex(openList, grid);// calculate it
+			PathNode currentNode = grid[currentNodeIndex]; // current node on the grid based on the calculation above
+
+			if(currentNodeIndex == endNodeIndex) {
+				// Reached our destination !
+				break;
+            }
+
+			//Remove currentNode from Open List
+			for ( int i=0; i< openList.Length; i++) {
+				if(openList[i] == currentNodeIndex) {
+					openList.RemoveAtSwapBack(i); 
+					break;
+                }
+            }
+
+			closedList.Add(currentNodeIndex);
+
+		// Neighbours!
+			for ( int i=0; i< neighbours.Length; i++) {
+				int2 neighbour = neighbours[i]; // get hold of each individual neighbour cell
+				int2 neighbourPosition = new int2(currentNode.x + neighbour.x, currentNode.y + neighbour.y); // look back at neighbours list to understand
+
+                if (!IsPositionInsideGrid(neighbourPosition, gridSize)) {
+					// Neighbour not valid position, skip
+					continue;
+                }
+
+				int neighbourIndex = CalculateIndex(neighbourPosition.x, neighbourPosition.y, gridSize.x);
+                if (closedList.Contains(neighbourIndex)) {
+					// Already searched this node, skip
+					continue;
+                }
+
+				PathNode neighbourNode = grid[neighbourIndex];
+                if (!neighbourNode.isWalkable) {
+					// Not walkable
+					continue;
+                }
+
+				int2 currentNodePosition = new int2(currentNode.x, currentNode.y);
+
+				//calculate algorithm
+				int tentativeGCost = currentNode.gCost + CalculateDistanceCost(currentNodePosition, neighbourPosition);// current node cost + distance cost from current node pos to neighbour
+				if(tentativeGCost < neighbourNode.gCost) { // it always kinda is lower because default gCost is 278248248274824 ( really big)
+					neighbourNode.cameFromIndex = currentNodeIndex;
+					neighbourNode.gCost = tentativeGCost;
+					neighbourNode.CalculateFCost();
+					grid[neighbourIndex] = neighbourNode;
+
+                    if (!openList.Contains(neighbourIndex)) {
+						openList.Add(neighbourNode.index);
+                    }
+
+					// End of Pathfinding! The outcome:  We either found the path or not.
+				}
+			}
+		}
+
+		// We`ve exhausted our openList, seached all, now what?
+		//cameFromNodeIndex will tell us !
+
+		PathNode endNode = grid[endNodeIndex];
+		if(endNode.cameFromIndex == -1) {
+			Debug.Log("Didn't find a path!");
+        } else {
+			// Found a path, use CalculateBackwardsPath()
+			NativeList<int2> backwardsPath = CalculateBackwardsPath(grid, endNode);
+
+			Debug.Log("Backwards Path ( from " + startPos + " to " + endPos);
+			foreach (int2 pathPosition in backwardsPath) {
+				
+				Debug.Log(pathPosition);
+            }
+
+			// Right now wedont care about getting the path back, we just want to calculate it, so dispose of it straight away;
+			backwardsPath.Dispose();
+		}
 
 
 		grid.Dispose();
+		neighbours.Dispose();
+		openList.Dispose();
+		closedList.Dispose();
 
 	}
 
@@ -95,13 +227,71 @@ public class Pathfinding : MonoBehaviour {
 	* int2 a - basically means int2 (a.x, a.y) - This node with x,y coords
 	* xDist, Y Dist, remaining and return - check drawn explanation if needed
 	*/
-	private int CalculateHCost(int2 A, int2 B) {
+	private int CalculateDistanceCost(int2 A, int2 B) {
 		int xDist = math.abs(A.x - B.x);
 		int yDist = math.abs(A.y - B.y);
 		int remaining = math.abs(xDist - yDist);
 
 		return MOVE_DIAGONAL_COST * math.min(xDist, yDist) + MOVE_STRAIGHT_COST * remaining; // explain all of it
     }
+
+	/* ------ Helper 3. GetLowestCostFNodeIndex ------- 
+	 * Add explanations
+	 */
+	private int GetLowestCostFNodeIndex(NativeList<int> openList, NativeArray<PathNode> grid ) {
+		PathNode lowestCostPathNode = grid[openList[0]]; // start from the beginning of openList
+
+		// Loop through the openList
+		for(int i =0; i< openList.Length; i++) {
+			PathNode testPathNode = grid[openList[i]];
+			if(testPathNode.fCost < lowestCostPathNode.fCost) {
+				lowestCostPathNode = testPathNode;
+            }
+        }
+
+		return lowestCostPathNode.index;
+    }
+
+	/* ------ Helper 4. IsPositionInsideGrid() ------- 
+	* Here we are checking if x and y coordinates are bigger than 0, 
+	* but lower than grid max coordinates, thus giving us valid cells.
+	*/
+	private bool IsPositionInsideGrid(int2 gridPosition, int2 gridSize) {
+		return
+			gridPosition.x >= 0 &&
+			gridPosition.y >= 0 &&
+			gridPosition.x < gridSize.x &&
+			gridPosition.y < gridSize.y;
+	}
+
+	/* ------ Helper 5. CalculatePath() -------
+	 * CalculatePath is a list of coordinates, it takes in the grid and the endNode.
+	 * Add detailed explanation.
+	 */
+	private NativeList<int2> CalculateBackwardsPath(NativeArray<PathNode> grid, PathNode endNode) {
+		if(endNode.cameFromIndex == -1) {
+			// Couldn't find a path, return an empty native list, so our int2 is going to be our path position;
+			return new NativeList<int2>(Allocator.Temp); 
+        } else {
+			// Found a path, need to walk backwards to get our actual path
+			NativeList<int2> path = new NativeList<int2>(Allocator.Temp);
+			path.Add(new int2(endNode.x, endNode.y)); // The path receives the end node location to begin with.
+
+			PathNode currentNode = endNode; // we start from endNode to go backwards
+			while(currentNode.cameFromIndex != -1) { // while we have a cameFromindex in our currentNode
+				PathNode cameFromNode = grid[currentNode.cameFromIndex]; // we grab the node in taht index
+				path.Add(new int2(cameFromNode.x, cameFromNode.y)); // get position
+				currentNode = cameFromNode; // and we add that position into our path
+			}
+
+			return path;
+
+		}
+
+
+		
+
+	}
 
 	/* ------------------1. Define A* Pathfinding data -----------------------------
 		gCost - Move cost from start node onto this node.
@@ -114,12 +304,17 @@ public class Pathfinding : MonoBehaviour {
 
 		public int x, y, index;
 		public int gCost, hCost, fCost;
-		public bool bIsWalkable;
+		public bool isWalkable;
 		public int cameFromIndex;
 
 		public void CalculateFCost() {
 			fCost = gCost + hCost;
         }
+
+		public void SetIsWalkable(bool isWalkable) {
+			this.isWalkable = isWalkable;
+		}
+
 	}
 
 }
