c0504b77f480c99a4e80517f715ae20f269f9527
A.Madalin <antonoiumadalin@gmail.com>
2020-07-09 14:29:56 +0300

    Intro to Dynamic Buffers

---
+++ Assets/3. Dynamic Buffers/IntBufferElement.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+
+/* ###
+ * IBufferElementData is the Unity interface that handles buffer elements 
+ */
+[InternalBufferCapacity(5)]
+public struct IntBufferElement : IBufferElementData {
+    public int Value;
+}
